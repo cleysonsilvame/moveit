c5198e7e1dc556c30a6ee5340ef8cf42dce95de1
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-02-23 17:43:15 -0300

    Components and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
+import React from 'react';
+import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+import { Countdown } from '../components/Countdown';
+
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+
+        <div></div>
+      </section>
     </div>
   );
 }
